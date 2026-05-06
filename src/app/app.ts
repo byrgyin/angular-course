a5786315cbceba3c2709f62be613a1d1dac59cd0
sergei_arkhipov_1 <byrgyin@yandex.ru>
2026-05-06 17:00:22 +0300

    comleate_condt_reder

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ProfileCard} from './common-ui/profile-card/profile-card';
+import {ProfileService} from './data/sevices/profile.service';
+import {Profile} from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProfileCard],
+  imports: [ProfileCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('test');
+  profileService = inject(ProfileService);
+  profiles:Profile[] = [];
+
+  constructor() {
+    this.profileService.getTestAccounts()
+      .subscribe(val => {
+      this.profiles = val;
+    })
+  }
 }
