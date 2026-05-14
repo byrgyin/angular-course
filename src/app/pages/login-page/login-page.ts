4d27a8dd76e78ee2c4315427046ee8d60627b8db
sergei_arkhipov_1 <byrgyin@yandex.ru>
2026-05-14 18:17:08 +0300

    learn_form

--- src/app/pages/login-page/login-page.ts
+++ src/app/pages/login-page/login-page.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule,} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone: true,
   templateUrl: './login-page.html',
   styleUrl: './login-page.scss',
+  imports: [
+    ReactiveFormsModule
+  ]
 })
 export class LoginPage {
-
+  form = new FormGroup({
+    username: new FormControl<string | null>(null),
+    password: new FormControl<string | null>(null),
+  })
+  onSubmit(event:Event){
+    console.log(event.target)
+  }
 }
